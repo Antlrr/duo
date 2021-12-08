2ddfd81303c6247c3fea0c2afad3da76ad4fea15
Antlrr <81817551+MrAntlrr@users.noreply.github.com>
2021-12-08 19:52:58 +0800

    maybe i should just not

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,10 @@
 Events.on(ClientLoadEvent, b  => {
 Vars.content.each(e => {
 if(!e.region) return;
-if(!e.rotatorRegion) return;
 if(!e.fullIcon) return;
 if(!e.uiIcon) return;
 e.region = Blocks.duo.region;
 e.fullIcon = Blocks.duo.region;
 e.uiIcon = Blocks.duo.region;
-e.rotatorRegion = Blocks.duo.region;
 });});
 //script by SMOLKEYS lol
