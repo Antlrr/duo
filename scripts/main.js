10706917df7185753d27357cc557744105405b50
Antlrr <81817551+MrAntlrr@users.noreply.github.com>
2021-12-08 19:41:04 +0800

    will this work

--- scripts/main.js
+++ scripts/main.js
@@ -3,8 +3,10 @@ Vars.content.each(e => {
 if(!e.region) return;
 if(!e.fullIcon) return;
 if(!e.uiIcon) return;
+if(!e.rotatorRegion) return;
 e.region = Blocks.duo.region;
 e.fullIcon = Blocks.duo.region;
 e.uiIcon = Blocks.duo.region;
+e.rotatorRegion = Blocks.duo.region;
 });});
 //script by SMOLKEYS lol
