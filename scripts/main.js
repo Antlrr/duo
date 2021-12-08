3b85d829393676b19a5c371f898b96f0925edf47
Antlrr <81817551+MrAntlrr@users.noreply.github.com>
2021-12-08 19:47:58 +0800

    ye

--- scripts/main.js
+++ scripts/main.js
@@ -4,11 +4,9 @@ if(!e.region) return;
 if(!e.rotatorRegion) return;
 if(!e.fullIcon) return;
 if(!e.uiIcon) return;
-if(!e.outlineRegion) return;
 e.region = Blocks.duo.region;
 e.fullIcon = Blocks.duo.region;
 e.uiIcon = Blocks.duo.region;
 e.rotatorRegion = Blocks.duo.region;
-e.outlineRegion = Blocks.duo.region
 });});
 //script by SMOLKEYS lol
